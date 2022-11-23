9cd87721bef902f037972f58f5ace72f5fc0c124
Solokhov Ildar <sir1504@mail.ru>
2022-11-23 21:14:36 +0300

    Some changes in smart contract

--- ethereum/contracts/Logistics.sol
+++ ethereum/contracts/Logistics.sol
@@ -2,10 +2,6 @@
 
 pragma solidity ^0.8.10;
 
-contract PushData {
-
-}
-
 contract Logistics {
     address owner;
     uint balanceReceived;
@@ -24,7 +20,7 @@ contract Logistics {
     Status status_choice;
 
     struct Admin {
-        string _name;
+        // string _name;
         bool _isAdmin;
     }
 
@@ -41,7 +37,9 @@ contract Logistics {
     }
 
     mapping(address => Admin) public admins;
-    Order[] public order_info;
+    // Order[] public order_info;
+    mapping(uint => Order) public order_info;
+
     mapping(uint => OrderPayments) public order_payments;
 
     event ChangeOrderStatus(uint indexed id, address indexed receiver, Status indexed product_status);
@@ -49,25 +47,42 @@ contract Logistics {
 
     function in_production(uint _orderID, address _receiver) public onlyOwner {
         status_choice = Status.In_Production;
-        order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+
+        order_info[_orderID]._orderID = _orderID;
+        order_info[_orderID]._receiver = _receiver;
+        order_info[_orderID]._status = status_choice;
+
+        // order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
         emit ChangeOrderStatus(_orderID, _receiver, status_choice);
     }
 
     function paid(uint _orderID, address _receiver) public onlyOwner {
         status_choice = Status.Paid;
-        order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        // order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        order_info[_orderID]._orderID = _orderID;
+        order_info[_orderID]._receiver = _receiver;
+        order_info[_orderID]._status = status_choice;
+
         emit ChangeOrderStatus(_orderID, _receiver, status_choice);
     }
 
     function in_stock(uint _orderID, address _receiver) public onlyOwner {
         status_choice = Status.In_Stock;
-        order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        // order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        order_info[_orderID]._orderID = _orderID;
+        order_info[_orderID]._receiver = _receiver;
+        order_info[_orderID]._status = status_choice;
+
         emit ChangeOrderStatus(_orderID, _receiver, status_choice);
     }
 
     function delivered(uint _orderID, address _receiver) public onlyOwner {
         status_choice = Status.Delivered;
-        order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        // order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        order_info[_orderID]._orderID = _orderID;
+        order_info[_orderID]._receiver = _receiver;
+        order_info[_orderID]._status = status_choice;
+
         if (_orderID == order_payments[_orderID]._orderID) {
             payable(owner).transfer(order_payments[_orderID]._amount);
         } else {
@@ -78,19 +93,31 @@ contract Logistics {
 
     function sent(uint _orderID, address _receiver) public onlyOwner {
         status_choice = Status.Sent;
-        order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        // order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        order_info[_orderID]._orderID = _orderID;
+        order_info[_orderID]._receiver = _receiver;
+        order_info[_orderID]._status = status_choice;
+
         emit ChangeOrderStatus(_orderID, _receiver, status_choice);
     }
 
     function canceled(uint _orderID, address _receiver) public onlyOwner {
         status_choice = Status.Canceled;
-        order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        // order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        order_info[_orderID]._orderID = _orderID;
+        order_info[_orderID]._receiver = _receiver;
+        order_info[_orderID]._status = status_choice;
+
         emit ChangeOrderStatus(_orderID, _receiver, status_choice);
     }
 
     function returned(uint _orderID, address _receiver) public onlyOwner {
         status_choice = Status.Returned_To_Warehouse;
-        order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        // order_info.push(Order({_orderID: _orderID, _receiver: _receiver, _status: status_choice}));
+        order_info[_orderID]._orderID = _orderID;
+        order_info[_orderID]._receiver = _receiver;
+        order_info[_orderID]._status = status_choice;
+
         emit ChangeOrderStatus(_orderID, _receiver, status_choice);
     }
 
